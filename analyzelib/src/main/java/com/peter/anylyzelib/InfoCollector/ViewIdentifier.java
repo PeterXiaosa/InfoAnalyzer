ab91b387061d0aa498f16fd19c68896f16682de8
Peter_Fu <fuhuicheng@esunny.cc>
2019-02-28 19:45:41 +0800

    1. Add the function of getViewId

--- analyzelib/src/main/java/com/peter/anylyzelib/InfoCollector/ViewIdentifier.java
+++ analyzelib/src/main/java/com/peter/anylyzelib/InfoCollector/ViewIdentifier.java
@@ -1,13 +1,41 @@
 package com.peter.anylyzelib.InfoCollector;
 
 import android.view.View;
+import android.view.ViewGroup;
 
 /**
  *
  */
 public class ViewIdentifier {
 
-    public static void getViewId(View view){
-
+    public static String getViewId(View view){
+        StringBuilder builder = new StringBuilder();
+        StringBuilder temp = new StringBuilder();
+        if (view != null){
+            do {
+                String viewType = view.getClass().getSimpleName();
+                int index = 0;
+                if ("DecorView".equals(viewType)){
+                    builder.insert(0, "DecorView");
+                    break;
+                }
+                ViewGroup parent = (ViewGroup) view.getParent();
+                for (int i = 0; i < parent.getChildCount(); i++){
+                    View child = parent.getChildAt(i);
+                    if (child == view){
+                        break;
+                    } else if (child.getClass().getSimpleName().equals(viewType)) {
+                        index++;
+                    }
+                }
+                builder.insert(0, temp.append("/")
+                        .append(viewType)
+                        .append("[")
+                        .append(index)
+                        .append("]"));
+                temp.delete(0, temp.length());
+            }while ((view = (View) view.getParent()) != null);
+        }
+        return builder.toString();
     }
 }
--- app/src/main/java/com/peter/infoanalyzer/MainActivity.java
+++ app/src/main/java/com/peter/infoanalyzer/MainActivity.java
@@ -4,41 +4,39 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatButton;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import com.peter.anylyzelib.InfoCollector.ViewIdentifier;
+
+import java.util.LinkedList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-    };
+    private Button btn_go;
+    private LinearLayout layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        btn_go = findViewById(R.id.btn_go);
+        layout = findViewById(R.id.layout);
+        btn_go.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("peterdebug", "view Id is " + ViewIdentifier.getViewId(btn_go));
+            }
+        });
+
+        AppCompatButton button
     }
 
 }
